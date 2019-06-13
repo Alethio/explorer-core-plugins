63cab5b3d93a64a6bdb1065f379f315c2389e7fb
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-06-13 14:12:38 +0300

    Fix block number field in suggest api result

--- src/app/eth-extended/data/search/ResultReader.ts
+++ src/app/eth-extended/data/search/ResultReader.ts
@@ -4,17 +4,30 @@ import { IResult } from "app/shared/data/search/IResult";
 import { ResultType } from "app/shared/data/search/ResultType";
 import { IUncleResultData } from "app/shared/data/search/result/IUncleResultData";
 import { ITxResultData } from "app/shared/data/search/result/ITxResultData";
+import { IBlockResultData } from "app/shared/data/search/result/IBlockResultData";
 
 export class ResultReader {
-    read(data: any, query: string) {
-        let type = readResultType(data["entity"]);
+    read(raw: any, query: string) {
+        let type = readResultType(raw["entity"]);
         let result: IResult = {
             type,
-            data: type === ResultType.Tx || type === ResultType.Uncle ? {
-                hash: "0x" + query.replace(/^0x/, "")
-            } as IUncleResultData | ITxResultData : data["data"]
+            data: this.readExtraData(type, raw["data"], query)
         };
 
         return result;
+    }
+
+    private readExtraData(type: ResultType, data: any, query: string) {
+        if (type === ResultType.Tx || type === ResultType.Uncle) {
+            return {
+                hash: "0x" + query.replace(/^0x/, "")
+            } as IUncleResultData | ITxResultData;
+        } else if (type === ResultType.Block) {
+            return {
+                blockNumber: data["number"]
+            } as IBlockResultData;
+        } else {
+            return data;
+        }
     }
 }
